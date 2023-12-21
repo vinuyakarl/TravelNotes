4948a602342ca04c11802d793d53b5c10bb092e7
Karl Vinuya <karl_vinuya@yahoo.ca>
2023-12-20 23:22:21 -0700

    Add base add trip fragment

--- app/src/main/java/com/example/travelnotes/main/entity/Trip.java
+++ app/src/main/java/com/example/travelnotes/main/entity/Trip.java
@@ -1,16 +1,23 @@
 package com.example.travelnotes.main.entity;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Trip {
     private String destination;
     private String origin;
     private UniqueID uniqueId;
+    private Date tripStarted;
+    private Date tripEnded;
+    private Float cost;
 
-    public Trip(String destination, String origin) {
+    public Trip(String destination, String origin, Date tripStarted, Date tripEnded, Float cost) {
         this.destination = destination;
         this.origin = origin;
         this.uniqueId = new UniqueID();
+        this.tripStarted = tripStarted;
+        this.tripEnded = tripEnded;
+        this.cost = cost;
     }
 
     public Trip() {
@@ -38,5 +45,29 @@ public class Trip {
 
     public void setUniqueId(UniqueID uniqueId) {
         this.uniqueId = uniqueId;
+    }
+
+    public Date getTripStarted() {
+        return tripStarted;
+    }
+
+    public void setTripStarted(Date tripStarted) {
+        this.tripStarted = tripStarted;
+    }
+
+    public Date getTripEnded() {
+        return tripEnded;
+    }
+
+    public void setTripEnded(Date tripEnded) {
+        this.tripEnded = tripEnded;
+    }
+
+    public Float getCost() {
+        return cost;
+    }
+
+    public void setCost(Float cost) {
+        this.cost = cost;
     }
 }
